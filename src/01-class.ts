c624b4c28ca38ac92bce28c0c6544145a696ec74
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-04-22 01:05:37 -0300

    Clase N°2: Class.

---
+++ src/01-class.ts
@@ -0,0 +1,26 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(2001, 1, 14);
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+// console.log({date, date2});
+
+class MyDate {
+    year: number;
+    month: number;
+    day: number;
+
+    constructor (year: number, month: number, day: number) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
+
+const myDate = new MyDate(2002, 2, 20);
+console.log({myDate});
